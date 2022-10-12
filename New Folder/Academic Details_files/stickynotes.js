e7652a91bccda64b63524821ecc31e46f404cba9
snehalpu <115658070+snehalpu@users.noreply.github.com>
2022-10-12 23:10:23 +0530

    Add files via upload

---
+++ New Folder/Academic Details_files/stickynotes.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+$("#notediv").hide();
+    $("body").height($(window).height());
+    $("body").delegate(".new-button","click",newNote);
+    $("body").delegate(".delete-button","click",deleteNote);
+    newNote();
+});
+
+function newNote() {
+    var temp = $("#note-template").html();
+    $("<div class='note'></div>").html(temp)
+                .appendTo("#note-space");
+}
+
+function deleteNote() {
+    //$(this).parents(".note").remove();
+    $("#notediv").hide();
+}
